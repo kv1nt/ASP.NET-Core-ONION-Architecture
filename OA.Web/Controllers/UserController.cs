558a4a4b3aaafb0c0d579211c2a9c05e1f65bfc3
Vitalii <kv111nt@gmail.com>
2019-01-12 22:37:43 +0200

    wqewd

---
+++ OA.Web/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using OA.Data;
+using OA.Repo;
+using OA.Service;
+using OA.Web.Models;
+
+namespace OA.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly IUserProfileService userProfileService;
+
+        public UserController(IUserService userService, IUserProfileService userProfileService)
+        {
+            this.userService = userService;
+            this.userProfileService = userProfileService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<UserViewModel> model = new List<UserViewModel>();
+            if (userService != null)
+            {
+                userService.GetUsers().ToList().ForEach(u =>
+                {
+                    UserProfile userProfile = userProfileService.GetUserProfile(u.Id);
+                    UserViewModel user = new UserViewModel
+                    {
+                        Id = u.Id,
+                        Name = $"{userProfile.FirstName} {userProfile.LastName}",
+                        Email = u.Email,
+                        Address = userProfile.Address
+                    };
+                    model.Add(user);
+                });
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ OA.Web/Models/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OA.Web.Models
+{
+    public class UserViewModel
+    {
+        [HiddenInput]
+        public Int64 Id { get; set; }
+
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [Display(Name = "Added Date")]
+        public DateTime AddedDate { get; set; }
+    }
+}
