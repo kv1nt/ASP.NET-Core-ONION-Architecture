c29ba3c461427ba63aabba10e7a946cd95dd9cdc
Vitalii <kv111nt@gmail.com>
2019-01-12 23:02:10 +0200

    Update UserController add addition CRUD Opreations

--- OA.Web/Controllers/UserController.cs
+++ OA.Web/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OA.Data;
 using OA.Repo;
@@ -40,6 +42,94 @@ namespace OA.Web.Controllers
             }
 
             return View(model);
+        }
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            UserViewModel model = new UserViewModel();
+
+            return PartialView("_AddUser", model);
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(UserViewModel model)
+        {
+            User userEntity = new User
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                Password = model.Password,
+                AddedDate = DateTime.UtcNow,
+                ModifiedDate = DateTime.UtcNow,
+                IPAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString(),
+                UserProfile = new UserProfile
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Address = model.Address,
+                    AddedDate = DateTime.UtcNow,
+                    ModifiedDate = DateTime.UtcNow,
+                    IPAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString()
+                }
+            };
+            userService.InsertUser(userEntity);
+            if (userEntity.Id > 0)
+            {
+                return RedirectToAction("index");
+            }
+            return View(model);
+        }
+
+        public ActionResult EditUser(int? id)
+        {
+            UserViewModel model = new UserViewModel();
+            if (id.HasValue && id != 0)
+            {
+                User userEntity = userService.GetUser(id.Value);
+                UserProfile userProfileEntity = userProfileService.GetUserProfile(id.Value);
+                model.FirstName = userProfileEntity.FirstName;
+                model.LastName = userProfileEntity.LastName;
+                model.Address = userProfileEntity.Address;
+                model.Email = userEntity.Email;
+            }
+            return PartialView("_EditUser", model);
+        }
+
+        [HttpPost]
+        public ActionResult EditUser(UserViewModel model)
+        {
+            User userEntity = userService.GetUser(model.Id);
+            userEntity.Email = model.Email;
+            userEntity.ModifiedDate = DateTime.UtcNow;
+            userEntity.IPAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            UserProfile userProfileEntity = userProfileService.GetUserProfile(model.Id);
+            userProfileEntity.FirstName = model.FirstName;
+            userProfileEntity.LastName = model.LastName;
+            userProfileEntity.Address = model.Address;
+            userProfileEntity.ModifiedDate = DateTime.UtcNow;
+            userProfileEntity.IPAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            userEntity.UserProfile = userProfileEntity;
+            userService.UpdateUser(userEntity);
+            if (userEntity.Id > 0)
+            {
+                return RedirectToAction("index");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public PartialViewResult DeleteUser(int id)
+        {
+            UserProfile userProfile = userProfileService.GetUserProfile(id);
+            string name = $"{userProfile.FirstName} {userProfile.LastName}";
+            return PartialView("_DeleteUser", name);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteUser(long id, FormCollection form)
+        {
+            userService.DeleteUser(id);
+            return RedirectToAction("Index");
         }
     }
 }
